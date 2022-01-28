c3cb1168ccf66c2a96adbc75a7dbe457b6d1bcc0
rased100 <rased100@gmail.com>
2022-01-29 02:25:50 +0600

    admin

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run() {
         await client.connect();
         const database = client.db('travelAgencyDB');
         const blogCollection = database.collection('blogs');
+        const usersCollection = database.collection('users');
 
         //GET Blogs API
         app.get('/blogs', async (req, res) => {
@@ -39,6 +40,42 @@ async function run() {
             const result = await blogCollection.insertOne(user);
             res.send(result);
         });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
 
     }
     finally {
